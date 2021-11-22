658a5c520127775aebda535f7999fddba64f6a67
pingu244 <pingu244@naver.com>
2021-11-22 18:42:10 +0900

    파베 연동

--- app/src/main/java/com/example/nabi/MainActivity.java
+++ app/src/main/java/com/example/nabi/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.nabi;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.nabi.fragment.Fragment_three;
@@ -59,6 +62,10 @@ public class MainActivity extends AppCompatActivity {
 
                 }
         );
+
+
+
+
 
 
     }
