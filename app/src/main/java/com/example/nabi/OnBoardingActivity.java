7510cf44a50991b1ee93f09fd4fa0022cb981e00
jungin-2 <ji991031@naver.com>
2022-05-05 21:49:56 +0900

    온보딩 완료

--- app/src/main/java/com/example/nabi/OnBoardingActivity.java
+++ app/src/main/java/com/example/nabi/OnBoardingActivity.java
@@ -1,5 +1,7 @@
 package com.example.nabi;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.rd.PageIndicatorView;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
@@ -23,7 +26,7 @@ public class OnBoardingActivity extends AppCompatActivity {
     ImageView imageView;
     ViewPager viewPager;
     LayoutInflater layoutInflater;
-    TabLayout tabLayout;
+    TabLayout  tabLayout;
     LinearLayout onBoardingLayout;
 
     public OnBoardingActivity(int position) {
@@ -55,14 +58,13 @@ public class OnBoardingActivity extends AppCompatActivity {
         onBoardingLayout = findViewById(R.id.onBoardingLayout);
 
 
-        tv_onBoarding = findViewById(R.id.tv_onBoarding);
-
         tv_pass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
+
             }
         });
 
@@ -70,82 +72,24 @@ public class OnBoardingActivity extends AppCompatActivity {
         tv_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(viewPager.getCurrentItem()==4){
-                    tv_pass.setVisibility(View.INVISIBLE);
+
+                if(viewPager.getCurrentItem()==0){
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+                }else if(viewPager.getCurrentItem()==1){
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+                }else if(viewPager.getCurrentItem()==2){
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
                     tv_next.setText("완료");
+                    tv_pass.setVisibility(View.INVISIBLE);
+                }else if(viewPager.getCurrentItem()==3){
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
-
     }
 
-    class OnBoardingAdapter extends PagerAdapter {
 
-        LayoutInflater layoutInflater;
-
-        public OnBoardingAdapter(LayoutInflater layoutInflater) {
-            this.layoutInflater = layoutInflater;
-        }
-
-        @Override
-
-        public int getCount() {
-
-            return 4; //이미지 개수 리턴
-
-        }
-
-
-        public Object instantiateItem(ViewGroup container, int position) {
-
-
-            View view = null;
-
-
-            view = layoutInflater.inflate(R.layout.onboarding, null);
-
-
-            if (position == 0) {
-                imageView.setImageResource(R.drawable.img_onboarding_1);
-                tv_onBoarding.setText("우울한 날씨를 확인하세요");
-
-            }
-            else if(position ==1){
-                imageView.setImageResource(R.drawable.img_onboarding_2);
-                tv_onBoarding.setText("힐링 컨텐츠로 힐링해 보아요");
-            }
-            else if(position==2){
-                imageView.setImageResource(R.drawable.img_onboarding_3);
-                tv_onBoarding.setText("나의 하루를 기록해 보아요");
-            }
-            else{
-                imageView.setImageResource(R.drawable.img_onboarding_4);
-                tv_onBoarding.setText("감정 보고서를 열람해 보세요");
-            }
-
-
-            container.addView(view);
-
-            return view;
-
-        }
-
-        @Override
-
-        public void destroyItem(ViewGroup container, int position, Object object) {
-
-            container.removeView((View) object);
-
-
-        }
-
-        @Override
-
-        public boolean isViewFromObject(View v, Object obj) {
-
-            return v == obj;
-
-        }
-
-    }
 }
