d07c0ebc75b2b2bd292bb71da3fd4b222e838c2b
000721 <hdhd0376@naver.com>
2022-05-08 02:39:54 +0900

    sad테스트화면 및 결과화면 디자인

--- app/src/main/java/com/example/nabi/fragment/Healing/SadTestResult.java
+++ app/src/main/java/com/example/nabi/fragment/Healing/SadTestResult.java
@@ -1,10 +1,12 @@
 package com.example.nabi.fragment.Healing;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +19,11 @@ import org.w3c.dom.Text;
 public class SadTestResult extends AppCompatActivity {
 
     ImageButton btnCancel;
+    ImageView resultIcon;
     Button btnReTest;
     TextView sadMention;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,19 +52,28 @@ public class SadTestResult extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         Intent receive_intent = getIntent();
         Integer count = receive_intent.getIntExtra("SadTestResult", 0);
 
         if (count>=3){
             result.setText("주의! 상담권유");
-            sadMention.setText(" 총 6개의 질문 중,"+count+"개의 질문에서 \"예\"를 체크하셨습니다.\n" +
-                    "  계절의 변화에 따른 신체 변화에 주의를 기울일 필요가 있어요.\n" +
-                    "  정도가 심하다면, 전문가와의 상담을 추천합니다.");
+            result.setTextColor(R.color.warning);
+            sadMention.setText("총 6개의 질문 중, "+count+"개의 질문에서 \"예\"를 체크하셨습니다.\n" +
+                    "계절의 변화에 따른 신체 변화에 주의를 기울일 필요가 있어요.\n" +
+                    "정도가 심하다면, 전문가와의 상담을 추천합니다.");
+            findViewById(R.id.sad_result_icon).setBackgroundResource(R.drawable.sad_warn);
         }
         else{
             result.setText("정상");
-            sadMention.setText(" 총 6개의 질문 중,"+count+"개의 질문에서 \"예\"를 체크하셨습니다.\n" +
-                    "  앞으로도 나비와 함께 힐링해봐요");
+            result.setTextColor(R.color.good);
+            sadMention.setText("총 6개의 질문 중, "+count+"개의 질문에서 \"예\"를 체크하셨습니다.\n" +
+                    "앞으로도 나비와 함께 힐링해봐요");
+            findViewById(R.id.sad_result_icon).setBackgroundResource(R.drawable.sad_good);
         }
-    }
+
+        }
+
+
 }
+
